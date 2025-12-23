3e7bf92b174bebef089cc67b913771fab8a6b14f
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-24 01:00:37 +0530

    Exploring Iterable interface methods 1- hasNext()

---
+++ 1- iterable/IteratorApp.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class IteratorApp{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		Iterator i=al.iterator();
+		
+		while(i.hasNext()){
+			Object obj=i.next();
+			System.out.println(obj);
+		}
+	}
+}
