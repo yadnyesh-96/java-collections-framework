cdb29615deb1817f6a44ec450f424625336341c8
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 15:25:54 +0530

    Collection Interface Method's 4-add(E): to add element in collection return type :true

---
+++ 2- collection/add_ColleApp.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class add_ColleApp{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		boolean b=al.add(10);
+		
+		if(b){
+			System.out.println("Element Added in ArrayList");
+		}else{
+			System.out.println("Element Not added in ArrayList");
+		}
+		
+	}
+}
