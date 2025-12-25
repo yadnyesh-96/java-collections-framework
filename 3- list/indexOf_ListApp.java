315eaefc6de98e6e26b15db6a719de5947d983a6
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 18:39:40 +0530

    List Interface and it's methods :4-indexOf(E)

---
+++ 3- list/indexOf_ListApp.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+
+public class indexOf_ListApp{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		System.out.println("index is :"+al.indexOf(20));
+		
+	}
+}
