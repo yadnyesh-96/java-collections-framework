55ea81b3f60500f5df6d0d2d2f7f34098f49aa09
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 18:30:48 +0530

    List Interface and it's methods :1-get(int)

---
+++ 3- list/get_ListApp.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class get_ListApp{
+	public static void main(String x[]){
+		
+		ArrayList al =new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		
+		System.out.println(al.get(1));
+	}
+}
