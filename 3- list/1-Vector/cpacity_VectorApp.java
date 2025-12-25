bb94e26a125b23a3bc29153d28403bbece0a966d
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 23:33:48 +0530

    Vectort is own method to know the capacity of vector : v.capacity()

---
+++ 3- list/1-Vector/cpacity_VectorApp.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class cpacity_VectorApp{
+	public static void main(String x[]){
+		
+		Vector v=new Vector();
+		
+		int capacity =v.capacity();
+		System.out.println("Capacity of the Vector is :"+capacity);
+		
+	}
+}
