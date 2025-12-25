39af34036d77fe026d4e014b5ca13657c3138840
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 18:33:03 +0530

    List Interface and it's methods :2-set(int,E)

---
+++ 3- list/set_ListApp.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class set_ListApp{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		
+		System.out.println(al.set(0,100));
+		System.out.println();
+		System.out.println(al);
+	}
+}
