e5fb8b761a18ef42ece1d5d7160fd0317b99f5de
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-27 12:32:01 +0530

    ArrayListQuestion solution -7

---
+++ 3- list/2-ArrayList/ArrayList_rmEven.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ArrayList_rmEven{
+	public static void main(String x[]){
+	Scanner sc=new Scanner(System.in);
+	CopyOnWriteArrayList al=new CopyOnWriteArrayList();
+	
+		al.add(40);
+		al.add(50);
+		al.add(20);
+		al.add(75);
+		al.add(10);
+		
+		System.out.println("ArrayList is:"+al);
+		
+		Iterator i=al.iterator();
+	
+		
+		while(i.hasNext()){
+			Object obj =i.next();
+			if((int)obj%2==0){
+				int index=al.indexOf((int)obj);
+				al.remove(index);
+			}
+		}
+		
+		System.out.println("After removed even elements ArrayList is:"+al);
+		
+	}
+}
