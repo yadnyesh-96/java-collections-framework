656daf1becab490b9b0f0efc4ac25567dcde6ca1
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-01 22:44:25 +0530

    Practice on the collections assignments

--- 3- list/2-ArrayList/remEven_App.java
+++ 3- list/2-ArrayList/remEven_App.java
@@ -5,16 +5,19 @@ public class remEven_App{
 	public static void main(String x[]){
 		int arr[]=new int[]{1,2,3,4,6,5};
 		
-		ArrayList<Integer> al=new ArrayList<>(Arrays.asList(arr));
+		ArrayList al=new ArrayList();
+		
+		for(int a:arr){
+			al.add(a);
+		}
 		
 		Iterator i=al.iterator();
 		
 		while(i.hasNext()){
-			
-			if(i.next()%2!=0){
-				System.out.println(i.next());
+			Object obj=i.next();
+			if((int)obj%2!=0){
+				System.out.println(obj);
 			}
-
 		}
 		
 	}
