cbf9cfa1e6646220506ac570790b443e480ed0bc
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-02 18:04:00 +0530

    Practice on the collection topics with map assignments

--- 6- map/Unique_MapAPP.java
+++ 6- map/Unique_MapAPP.java
@@ -1,11 +1,5 @@
 
-/*
-Q4. Take an integer array with repeated values. Store it in an ArrayList and print the frequency of each unique number.
-What you practice:
-Traversing ArrayList
-Using another ArrayList to track visited elements
-Logical counting
-*/
+
 
 import java.util.*;
 public class Unique_MapAPP{
