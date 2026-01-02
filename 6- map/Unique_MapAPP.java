5bbf4aa82bfb73064a2e7aa2eed6391c1b063393
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-02 18:30:04 +0530

    practice on the daily collection topics

--- 6- map/Unique_MapAPP.java
+++ 6- map/Unique_MapAPP.java
@@ -5,18 +5,23 @@ import java.util.*;
 public class Unique_MapAPP{
 		public static void main(String x[]){
 			
-			int a[]=new int[]{1,2,2,3,4,5,5};
+			int a[]=new int[]{1,1,1,2,2,3};
 			
 			HashMap map=new HashMap();
+			ArrayList al=new ArrayList();
 			
 			for(int v:a){
 				if(map.containsKey(v)){
 					map.put(v, (int)map.get(v) + 1);
 				}else{
 					map.put(v,1);
+					al.add(v);
 				}
 			}
 			
 			System.out.println(map);
+			
+			
+			System.out.println(al);
 		}
 }
