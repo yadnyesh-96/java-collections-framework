977490e8a5d16e370297425d7fb0976c137f8a01
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-14 21:39:56 +0530

    Practce of Vector Assignments

--- 3- list/1-Vector/ArrayListToVector.java
+++ 3- list/1-Vector/ArrayListToVector.java
@@ -21,7 +21,7 @@ public class ArrayListToVector{
 		Vector v=new Vector(al);
 		
 		for(int i=0; i<v.size(); i++){
-			// for(int j=i+1; j<v.size(); j++){
+			for(int j=i+1; j<v.size(); j++){
 				
 				int val1 = (int) v.get(i);
                 int val2 = (int) v.get(j);
