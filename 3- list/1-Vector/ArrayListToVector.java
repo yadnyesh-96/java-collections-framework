5626bf9f01b6e626613184e1e2bc6d41438b9fb7
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-14 21:46:31 +0530

    Practce of Vector Assignments

--- 3- list/1-Vector/ArrayListToVector.java
+++ 3- list/1-Vector/ArrayListToVector.java
@@ -11,12 +11,12 @@ public class ArrayListToVector{
 	public static void main(String x[]){
 		ArrayList al=new ArrayList();
 		
-		al.add(50);
-		al.add(10);
-		al.add(30);
-		al.add(10);
-		al.add(20);
-		al.add(40);
+		// al.add(50);
+		// al.add(10);
+		// al.add(30);
+		// al.add(10);
+		// al.add(20);
+		// al.add(40);
 		
 		Vector v=new Vector(al);
 		
