2b4bdd01a65b29ea98c5760f8b9dba371b37da70
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-23 23:45:03 +0530

    collection practice

--- 3- list/1-Vector/cpacity_VectorApp.java
+++ 3- list/1-Vector/cpacity_VectorApp.java
@@ -1,10 +1,7 @@
 
 import java.util.*;
 
-public class cpacity_VectorApp{
-	public static void main(String x[]){
-		
-		Vector v=new Vector();
+public class cpacity_VectorApp{\
 		
 		int capacity =v.capacity();
 		System.out.println("Capacity of the Vector is :"+capacity);
