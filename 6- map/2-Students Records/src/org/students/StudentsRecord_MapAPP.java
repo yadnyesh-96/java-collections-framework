dabc28ba5980c019de51219c806ccdf414338985
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-31 22:19:30 +0530

    Map Inteface in Java

--- 6- map/2-Students Records/src/org/students/StudentsRecord_MapAPP.java
+++ 6- map/2-Students Records/src/org/students/StudentsRecord_MapAPP.java
@@ -22,4 +22,23 @@ public class StudentsRecord_MapAPP {
 		Dept.put("FE", FE);
 		Dept.put("SE", SE);
 		
+		Set<Map.Entry>set=Dept.entrySet();
+		for(Map.Entry e:set) {
+			String Dname=e.getKey().toString();
+			System.out.println("----");
+			System.out.println("|"+Dname+"|");
+			System.out.println("----");
+			Object obj=e.getValue();
+			LinkedHashMap m=(LinkedHashMap)obj;
+			Set<Map.Entry>s=m.entrySet();
+			System.out.println("ID\tNAME");
+			System.out.println("-----------------------");
+			for(Map.Entry E:s) {
+				System.out.println(E.getKey()+"\t"+E.getValue());
+			}
+			System.out.println("-----------------------");
+			
+		}
+	}
 
+}
