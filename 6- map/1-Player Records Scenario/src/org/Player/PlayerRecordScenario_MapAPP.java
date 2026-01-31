18866f0b10bd1d7a201d85c7835599697a74be59
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-31 22:16:57 +0530

    Map Inteface in Java

--- 6- map/1-Player Records Scenario/src/org/Player/PlayerRecordScenario_MapAPP.java
+++ 6- map/1-Player Records Scenario/src/org/Player/PlayerRecordScenario_MapAPP.java
@@ -1,6 +1,17 @@
 package org.Player;
 
-
+import java.util.*;
+public class PlayerRecordScenario_MapAPP {
+	public static void main(String x[]) {
+		LinkedHashMap map=new LinkedHashMap();
+		
+			ArrayList al=new ArrayList();
+			
+			al.add(new Player(1,"ABC",15));
+			al.add(new Player(2,"MNO",35));
+			al.add(new Player(3,"PQR",45));
+			al.add(new Player(4,"SUV",10));
+			al.add(new Player(5,"XYZ",50));
 			
 			ArrayList al1=new ArrayList();
 			al1.add(new Player(1,"Virat",120));
