5e9bcd5db75f708129d5f6afe1fe9a70c9f16435
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-31 22:16:46 +0530

    Map Inteface in Java

--- 6- map/1-Player Records Scenario/src/org/Player/PlayerRecordScenario_MapAPP.java
+++ 6- map/1-Player Records Scenario/src/org/Player/PlayerRecordScenario_MapAPP.java
@@ -20,6 +20,19 @@ public class PlayerRecordScenario_MapAPP {
 			al1.add(new Player(4,"Shivam",120));
 			al1.add(new Player(5,"Malinga",150));
 
+			map.put("IND",al1);
+			map.put("ASUS", al);
 			
+			Set<Map.Entry>set=map.entrySet();
+			for(Map.Entry e:set) {
+				String Tname=e.getKey().toString();
+				System.out.println(Tname);
+				Object obj=e.getValue();
+				ArrayList val=(ArrayList)obj;
+				for(Object o:val) {
+					Player p=(Player)o;
+					System.out.println(p.getId()+"\t"+p.getName()+"\t"+p.getRun());
+				}
+			}
 	}
 }
