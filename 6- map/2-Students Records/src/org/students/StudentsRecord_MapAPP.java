22d2e1f41c787273d1495d07861ed94cffe15b95
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-31 22:19:45 +0530

    Map Inteface in Java

--- 6- map/2-Students Records/src/org/students/StudentsRecord_MapAPP.java
+++ 6- map/2-Students Records/src/org/students/StudentsRecord_MapAPP.java
@@ -1,44 +1,44 @@
-// package org.students;
-// import java.util.*;
-// public class StudentsRecord_MapAPP {
+package org.students;
+import java.util.*;
+public class StudentsRecord_MapAPP {
 
-// 	public static void main(String[] args) {
-// 		LinkedHashMap FE=new LinkedHashMap();
+	public static void main(String[] args) {
+		LinkedHashMap FE=new LinkedHashMap();
 		
-// 		FE.put(1,"Anurag");
-// 		FE.put(2, "Harshal");
-// 		FE.put(3, "Saurabh");
-// 		FE.put(4, "Nikhil");
-// 		FE.put(5, "Mangesh");
+		FE.put(1,"Anurag");
+		FE.put(2, "Harshal");
+		FE.put(3, "Saurabh");
+		FE.put(4, "Nikhil");
+		FE.put(5, "Mangesh");
 		
-// 		LinkedHashMap SE=new LinkedHashMap();
-// 		SE.put(1,"Aniket");
-// 		SE.put(2,"Shubham");
-// 		SE.put(3,"Abhijeet");
-// 		SE.put(4,"Pranav");
-// 		SE.put(5,"Sachin");
+		LinkedHashMap SE=new LinkedHashMap();
+		SE.put(1,"Aniket");
+		SE.put(2,"Shubham");
+		SE.put(3,"Abhijeet");
+		SE.put(4,"Pranav");
+		SE.put(5,"Sachin");
 		
-// 		LinkedHashMap Dept=new LinkedHashMap();
-// 		Dept.put("FE", FE);
-// 		Dept.put("SE", SE);
+		LinkedHashMap Dept=new LinkedHashMap();
+		Dept.put("FE", FE);
+		Dept.put("SE", SE);
 		
-// 		Set<Map.Entry>set=Dept.entrySet();
-// 		for(Map.Entry e:set) {
-// 			String Dname=e.getKey().toString();
-// 			System.out.println("----");
-// 			System.out.println("|"+Dname+"|");
-// 			System.out.println("----");
-// 			Object obj=e.getValue();
-// 			LinkedHashMap m=(LinkedHashMap)obj;
-// 			Set<Map.Entry>s=m.entrySet();
-// 			System.out.println("ID\tNAME");
-// 			System.out.println("-----------------------");
-// 			for(Map.Entry E:s) {
-// 				System.out.println(E.getKey()+"\t"+E.getValue());
-// 			}
-// 			System.out.println("-----------------------");
+		Set<Map.Entry>set=Dept.entrySet();
+		for(Map.Entry e:set) {
+			String Dname=e.getKey().toString();
+			System.out.println("----");
+			System.out.println("|"+Dname+"|");
+			System.out.println("----");
+			Object obj=e.getValue();
+			LinkedHashMap m=(LinkedHashMap)obj;
+			Set<Map.Entry>s=m.entrySet();
+			System.out.println("ID\tNAME");
+			System.out.println("-----------------------");
+			for(Map.Entry E:s) {
+				System.out.println(E.getKey()+"\t"+E.getValue());
+			}
+			System.out.println("-----------------------");
 			
-// 		}
-// 	}
+		}
+	}
 
-// }
+}
